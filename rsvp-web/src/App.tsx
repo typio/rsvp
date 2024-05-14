7d44f57012a74affc9f2382778ebb9bb23735005
Thomas Huber <me@tohuber.com>
2024-05-14 03:38:21 -0700

    Add some of date select UI

--- rsvp-web/src/App.tsx
+++ rsvp-web/src/App.tsx
@@ -7,14 +7,45 @@ import Footer from './Footer'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [daysEnabled, setDaysEnabled] = useState({
+    sunday: false,
+    monday: true,
+    tuesday: true,
+    wednesday: true,
+    thursday: true,
+    friday: true,
+    saturday: false
+  })
 
   return (
     <>
       <div className="py-8 px-8 min-h-[100vh] grid grid-rows-[auto_1fr_auto] gap-y-8">
         <Header />
         <main className="grid grid-cols-2 gap-x-8">
-          <div className="flex flex-row">
-            <DatePickerWithRange />
+          <div className="flex flex-col">
+            <div className="flex flex-row">
+              <DatePickerWithRange />
+            </div>
+            <div className="flex flex-row gap-x-2 my-2">
+              {Object.entries(daysEnabled).map(([day, dayEnabled], i) => {
+                return (
+                  <Button
+                    key={i}
+                    className={`${dayEnabled ? 'bg-primary text-primary-foreground hover:bg-primary/80' : 'border-2 border-primary bg-background text-foreground hover:bg-background/70'} rounded-sm w-11 h-11`}
+                    onClick={() => {
+                      setDaysEnabled({ ...daysEnabled, [day]: !dayEnabled })
+                    }}
+                  >
+                    {day
+                      .slice(0, 3)
+                      .split('')
+                      .map((l: string, i: number) =>
+                        i === 0 ? l.toUpperCase() : l
+                      )}
+                  </Button>
+                )
+              })}
+            </div>
           </div>
           <div className="flex flex-row">
             <Button onClick={() => setCount(count => count + 1)}>
